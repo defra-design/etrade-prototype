c239106dbd18b581f0f9e894afd42067a902b3ce
jonnow <jonnowitts@gmail.com>
2022-01-20 17:31:30 +0000

    Logic for show/hiding step by step sections. Changed the getting started guide with new content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,48 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
-})
+
+  $('#defra-step-nav .defra-step-nav--panel').addClass('panel--is-closed');
+  $('#defra-step-nav--show-all').addClass('hide-all')
+
+  $('#defra-step-nav--show-all').click(function(e){
+    if($(this).hasClass('hide-all')) {
+      $('.defra-step-nav--panel').removeClass('panel--is-closed');
+      $(this).removeClass('hide-all');
+    }
+    else {
+      $('.defra-step-nav--panel').addClass('panel--is-closed');
+      $(this).addClass('hide-all');
+    }
+  });
+
+  $('#defra-step-nav-explore-apis-1').click(function(e){
+    togglePanel(this.id);
+  });
+
+  $('#defra-step-nav-review-api-documentation-2').click(function(e){
+    togglePanel(this.id);
+  });
+
+  $('#defra-step-nav-create-account-3').click(function(e){
+    togglePanel(this.id);
+  });
+
+  $('#defra-step-nav-view-sample-project-4').click(function(e){
+    togglePanel(this.id);
+  });
+  
+  $('#defra-step-nav-view-sample-project-5').click(function(e){
+    togglePanel(this.id);
+  });
+  
+  $('#defra-step-nav-view-sample-project-6').click(function(e){
+    togglePanel(this.id);
+  });
+});
+
+function togglePanel(stepId) {
+  var panelId = '#' + stepId + '--panel';
+  $(panelId).toggleClass('panel--is-closed');
+  return;
+}
