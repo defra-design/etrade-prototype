87f765181b352c0c20c9901e8c68e12dbd12635d
jonnow <jonnowitts@gmail.com>
2022-01-21 15:38:44 +0000

    Applied styling and logic to open closed on get started page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,43 +12,46 @@ $(document).ready(function () {
   $('#defra-step-nav--show-all').addClass('hide-all')
 
   $('#defra-step-nav--show-all').click(function(e){
+    // show
     if($(this).hasClass('hide-all')) {
-      $('.defra-step-nav--panel').removeClass('panel--is-closed');
       $(this).removeClass('hide-all');
+      $(this).children('.defra-step-nav--chevron').addClass('flipped');
+      $('.defra-step-nav--show-all-label').text('Hide all steps');
+      $('.defra-step-nav--header .defra-step-nav--toggle .visible').text('Hide');
+      $('.defra-step-nav--header .defra-step-nav--chevron').addClass('flipped');
+      $('.defra-step-nav--panel').removeClass('panel--is-closed');
     }
+
+    // hide
     else {
-      $('.defra-step-nav--panel').addClass('panel--is-closed');
       $(this).addClass('hide-all');
+      $(this).children('.defra-step-nav--chevron').removeClass('flipped');
+      $('.defra-step-nav--show-all-label').text('Show all steps');
+      $('.defra-step-nav--header .defra-step-nav--toggle .visible').text('Show');
+      $('.defra-step-nav--header .defra-step-nav--chevron').removeClass('flipped');
+      $('.defra-step-nav--panel').addClass('panel--is-closed');
     }
   });
 
-  $('#defra-step-nav-explore-apis-1').click(function(e){
-    togglePanel(this.id);
-  });
-
-  $('#defra-step-nav-review-api-documentation-2').click(function(e){
-    togglePanel(this.id);
-  });
-
-  $('#defra-step-nav-create-account-3').click(function(e){
-    togglePanel(this.id);
-  });
-
-  $('#defra-step-nav-view-sample-project-4').click(function(e){
-    togglePanel(this.id);
-  });
-  
-  $('#defra-step-nav-view-sample-project-5').click(function(e){
-    togglePanel(this.id);
-  });
-  
-  $('#defra-step-nav-view-sample-project-6').click(function(e){
+  $('.defra-step-nav--step').click(function(e){
     togglePanel(this.id);
   });
 });
 
 function togglePanel(stepId) {
-  var panelId = '#' + stepId + '--panel';
-  $(panelId).toggleClass('panel--is-closed');
+  var str_stepId = '#' + stepId,
+      str_panelId = '#' + stepId + '--panel',
+      str_toggle = str_stepId + ' .defra-step-nav--toggle .visible',
+      str_chevron = str_stepId + ' .defra-step-nav--chevron';
+      
+  $(str_panelId).toggleClass('panel--is-closed');
+  $(str_chevron).toggleClass('flipped');
+
+  if($(str_chevron).hasClass('flipped')) {
+    $(str_toggle).text('Hide');
+  }
+  else {
+    $(str_toggle).text('Show');
+  }
   return;
 }
