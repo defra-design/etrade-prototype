04f14fc022d059eda56cdcfc07163943224a14d5
jonnow <jonnowitts@gmail.com>
2022-01-27 14:49:53 +0000

    Updates to routing and folder names and locations. Added catch for any old links to forward to new i.e. api-step-by-step => documentation

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,12 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Catch any missed renames
+router.get('/homepage-v2', (req, res) => {
+    res.redirect('homepage');
+});
+
+router.get('/api-step-by-step/introduction', (req, res) => {
+    res.redirect('/documentation/introduction');
+})
