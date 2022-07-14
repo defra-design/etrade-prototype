938bd9f2858f5e9b13509d950c991788fb8991a8
Brian Coult <you@example.com>
2022-07-14 09:08:11 +0100

    routes files

--- app/routes/apiCredentials.js
+++ app/routes/apiCredentials.js
@@ -2,14 +2,16 @@ const { generateRandomString } = require('../utils/helpers')
 
 module.exports = router => {
   router.all([
-    '/account/manage-applications/:id',
-    '/account/manage-applications/:id/*'    
+    '/account/production-credentials/:id',
+    '/account/production-credentials/:id/*',    
+    
   ], (req, res, next) => {
     const data = req.session.data
     res.locals.id = req.params.id
-    res.locals.sandapp = data.sandapps && data.sandapps[req.params.id]
+    res.locals.credential = data.credentials && data.credentials[req.params.id]
     next()
   })
 
+  
 
 }
