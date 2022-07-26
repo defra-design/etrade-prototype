43af42d85c65fa67ed0da0fa79ae3ebad4d4cf94
Brian Coult <you@example.com>
2022-07-26 21:57:30 +0100

    product credentials link up and applications details page

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -2,8 +2,12 @@ const { generateRandomString } = require('../utils/helpers')
 
 module.exports = router => {
   router.all([
+    '/account/applications/:id',
+    '/account/applications/:id/*',    
     '/account/applications/sandbox/:id',
     '/account/applications/sandbox/:id/*',    
+    '/account/applications/production-credentials/:id',
+    '/account/applications/production-credentials/:id/*',    
     
   ], (req, res, next) => {
     const data = req.session.data
@@ -17,32 +21,46 @@ module.exports = router => {
     next()
   })
 
-  // Generates new application ID for a sandbox
+  // Generates new application ID for an application in sandbox which is default
   router.all('/account/applications/sandbox/add', (req, res) => {
     res.redirect(`/account/applications/sandbox/${generateRandomString()}/add/name`)
   })
 
-  router.all('/account/applications/sandbox/:id/add/:view', (req, res, next) => {    
-    // console.log(sandboxApps)
-    // console.log(Object.values(sandboxApps))
+  router.all('/account/applications/sandbox/:id/add/:view', (req, res, next) => {        
     res.render(`account/applications/sandbox/add/${req.params.view}`)    
   })
-
-
-  // List out applications
-  // router.all('/account/applications', (req, res, next) => {
-  //   const applications = req.session.data.applications
-  //   res.locals.hasAnyApps = Object.values(applications).some((p) => p.status === 'Contacted' )
-  //   next()
-  // })
-
 
   // User validation
   router.all('/account/applications', (req, res, next) => {
     const applications = req.session.data.applications
-    res.locals.hasAnyContacted = Object.values(applications).some((p) => p.status === 'Contacted' )
+    res.locals.hasAnyApps = Object.values(applications).some((p) => p.name !== '' )
     next()
   })
+
+
+  // Details page for an application
+  router.all('/account/applications/:id', (req, res) => {
+    res.render('account/applications/details')
+  })
+
+  router.all('/account/applications/:id/details/:view', (req, res) => {
+    res.render(`account/applications/details/${req.params.view}`)
+  })
+
+
+  // Apply for product credentials
+  router.all('/account/applications/:id/production-credentials/', (req, res) => {
+    res.render('account/applications/production-credentials/index')
+  })
+
+  router.all('/account/applications/:id/production-credentials/:view', (req, res) => {
+    res.render(`account/applications/production-credentials/${req.params.view}`)
+  })
+
+
+
+
+
 
 
 
