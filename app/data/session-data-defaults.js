285c0ba2b18d255877f9a341790d263014a340a5
Brian Coult <briancoult@MacMiniefraWork.localdomain>
2022-08-25 15:16:51 +0100

    redirect data and logic to show on details page

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,16 +19,76 @@ Example usage:
 module.exports = {
   
   applications: {
-    ME1: { name: 'Bird', apis: "Export application" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' },
-    GH3: { name: 'Magic', apis: "Fish Export Service (FES)" , redirectURI: "exampleTest.com", production: "Yes", status: 'Contacted' },
-    JH2: { name: 'Curry', apis: "Import of products, animals, food and feed system" , redirectURI: "exampleTest.com", production: "Yes", status: 'Contacted' },
-    LK9: { name: 'Chamberlain', apis: "Export application" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' },
-    NB7: { name: 'LeBron', apis: "Import of products, animals, food and feed systemn" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' },    
-    SA1: { name: 'Jordan', apis: "Export application, Import of products, animals, food and feed system" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' },
-    DF9: { name: 'Pippen', apis: "Export application" , redirectURI: "exampleTest.com", production: "Yes", status: 'Contacted' },
-    JR6: { name: 'Bryant', apis: "Fish Export Service (FES)n" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' },
-    OQ4: { name: 'Erving', apis: "Fish Export Service (FES)" , redirectURI: "exampleTest.com", production: "Yes", status: 'Contacted' },
-    PL7: { name: 'Rodman', apis: "Import of products, animals, food and feed system" , redirectURI: "exampleTest.com", production: "No", status: 'Contacted' }
+    ME1: { 
+      name: 'Bird', 
+      apis: "Export application" , 
+      redirectURI: "larrybird.test.com", 
+      production: "No", 
+      status: 'Contacted' 
+    },
+    GH3: { 
+      name: 'Magic', 
+      apis: "Fish Export Service (FES)" , 
+      redirectURI: "magicjohnson.test.com", 
+      production: "Yes", 
+      status: 'Contacted' 
+    },
+    JH2: { 
+      name: 'Curry', 
+      apis: "Import of products, animals, food and feed system" , 
+      redirectURI: "stephcurry.test.com", 
+      production: "Yes", 
+      status: 'Contacted' 
+    },
+    LK9: { 
+      name: 'Chamberlain', 
+      apis: "Export application" , 
+      redirectURI: "willchamberlain.test.com", 
+      production: "No", 
+      status: 'Contacted' 
+    },
+    NB7: { 
+      name: 'LeBron', 
+      apis: "Import of products, animals, food and feed systemn" , 
+      redirectURI: "lebronhames.test.com", 
+      production: "No", 
+      status: 'Contacted' },  
+
+    SA1: { 
+      name: 'Jordan', 
+      apis: "Export application, Import of products, animals, food and feed system" , 
+      redirectURI: "michaeljordan.test.com", 
+      production: "No", 
+      status: 'Contacted' 
+    },
+    DF9: { 
+      name: 'Pippen', 
+      apis: "Export application" , 
+      redirectURI: "scottiepippen.test.com", 
+      production: "Yes", 
+      status: 'Contacted' 
+    },
+    JR6: { 
+      name: 'Bryant', 
+      apis: "Fish Export Service (FES)n" , 
+      redirectURI: "kobbryant.test.com", 
+      production: "No", 
+      status: 'Contacted' 
+    },
+    OQ4: { 
+      name: 'Erving', 
+      apis: "Fish Export Service (FES)" , 
+      redirectURI: "juliuserving.test.com", 
+      production: "Yes", 
+      status: 'Contacted' 
+    },
+    PL7: { 
+      name: 'Rodman', 
+      apis: "Import of products, animals, food and feed system" , 
+      redirectURI: "dennisrodman.test.com", 
+      production: "No", 
+      status: 'Contacted'
+     }
   }
 
 }
