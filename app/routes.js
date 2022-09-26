2b8db05b8abf73e1bb038b5687fd53719840359c
Brian Coult <Brian.coult@defra.gov.uk>
2022-09-26 13:09:46 +0100

    Delete app directory

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Add your routes here - above the module.exports line
-require('./routes/prodcredentials')(router)
-require('./routes/applications')(router)
-
-
-module.exports = router
