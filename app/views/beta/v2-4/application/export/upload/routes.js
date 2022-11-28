c31178f6b57e9af1a961150ec61d2add9c34bc6f
Dennis Flood <dennis.flood@gmail.com>
2022-11-28 14:09:11 +0000

    Added check for empty array

--- app/views/beta/v2-4/application/export/upload/routes.js
+++ app/views/beta/v2-4/application/export/upload/routes.js
@@ -435,9 +435,15 @@ module.exports = function(router) {
                       } else if (cert_schema[c].multiple === "yes") {
                         console.log("Found a multiple: " + cert_schema[c].id + " -- " + temp[cert_schema[c].id]);
                         // split previously stored value for this field into an array
-                        stringArray = temp[cert_schema[c].id].toString().split(",");
-                        // remove extraneous spaces
 
+                        if (temp[cert_schema[c].id] === null) {
+                          stringArray = [];
+                        } else {
+                          stringArray = temp[cert_schema[c].id].toString().split(",");
+
+                        }
+
+                        // remove extraneous spaces
                         for (x = 0; x < stringArray.length; x++) {
                           stringArray[x] = stringArray[x].trim();
                         }
