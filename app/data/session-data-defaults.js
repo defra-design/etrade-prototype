a7f2178d2f1eb57f8de66d64bc1d59ba77f3b81a
Dennis Flood <dennis.flood@gmail.com>
2023-01-04 11:19:57 +0000

    133105: Region of origin updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -55,5 +55,6 @@ module.exports = {
   "displayMax": 30,
   "paginationMax": 3,
   "manualSelection": ["not-selected"],
-  "person": {consignor : "0"}
+  "person": {consignor : "0"},
+  "transport": {country : "" }
 }
