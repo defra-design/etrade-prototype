8e33c8de0391a6f707eb968b393d2e9f04e6e6d8
Dennis Flood <dennis.flood@gmail.com>
2023-01-24 15:16:08 +0000

    Default consignor activity control

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -60,6 +60,6 @@ module.exports = {
   "displayMax": 30,
   "paginationMax": 3,
   "manualSelection": ["not-selected"],
-  "person": {consignor : "0"},
+  "person": {consignor : "0", consignorActivity : "1"},
   "transport": {country : "" }
 }
